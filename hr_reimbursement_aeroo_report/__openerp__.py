e547c57b03c4c991af82655c1298cdab4f44ad8c
Nurazmi <azmimr67@gmail.com>
2021-08-14 16:06:01 +0700

    [hr_reimbursement_aeroo_report] 8.0.1.0.0

---
+++ hr_reimbursement_aeroo_report/__openerp__.py
@@ -0,0 +1,27 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author/
+{
+    "name": "Employee Reimbursement Aeroo Report",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "depends": [
+        "hr_reimbursement",
+        "report_aeroo",
+        "base_amount_to_text",
+        "base_print_policy",
+    ],
+    "data": [
+        "reports/report_hr_reimbursement.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "AGPL-3",
+}
